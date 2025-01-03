ea95574c8505ce80d7ec5053e4cdc08bacbda92d
Daniel Christ Winata <danielchrist.winata@itbss.ac.id>
2025-01-03 14:40:39 +0700

    add Admin TableDosen dan TableMatkul edit dan hapus

--- blinky-fe/src/pages/Admin/TableDosen.vue
+++ blinky-fe/src/pages/Admin/TableDosen.vue
@@ -7,6 +7,7 @@
 
     const dataDosen = ref()
     const dataProdi = ref()
+    const temp_data = ref()
 
     const nama = ref()
     const prodi = ref("0")
@@ -16,10 +17,12 @@
     const password = ref()
 
     const isModalOn = ref(false)
+    const modalMode = ref('Add')
 
     async function fetchData(){
         await apiClient.get('dosen')
         .then(resp => {
+            console.log(resp.data)
             dataDosen.value = resp.data
         })
 
@@ -37,7 +40,7 @@
                 'jabatan': jabatan.value,
                 'nidn': nidn.value,
                 'email': email.value,
-                'password': password.value
+                'password': password.value,
             }
         })
         .then(resp => {
@@ -52,8 +55,61 @@
         })
     }
 
-    function toggleModal(val){
+    async function updateDosen(){
+        await apiClient.get('dosen/update', {
+            params: {
+                'id_dosen' : temp_data.value.id_dosen,
+                'id_prodi' : prodi.value,
+                'nm_dosen' : nama.value,
+                'jabatan': jabatan.value,
+                'nidn': nidn.value,
+                'email': email.value,
+                'password': password.value,
+            }
+        })
+        .then(resp => {
+            console.log(resp.data)
+
+            toggleModal(0, 'Edit')
+        })
+    }
+
+    async function hapusDosen(x){
+        await apiClient.get('dosen/delete', {
+            params: {
+                'id_dosen': x.id_dosen,
+            }
+        })
+        .then(resp => {
+            if(resp.data == "Success!"){
+                alert('Data dosen berhasil dihapus!')
+                fetchData();
+            }
+            else{
+                alert('Data dosen gagal dihapus!')
+            }
+
+            toggleModal(0, 'Delete')
+        })
+    }
+
+    function toggleModal(val, mode){
         isModalOn.value = !isModalOn.value
+        modalMode.value = mode
+
+        temp_data.value = val
+
+        if(mode == 'Edit'){
+            prodi.value = val.id_prodi
+            nama.value = val.nm_dosen
+            jabatan.value = val.jabatan
+            nidn.value = val.nidn
+            email.value = val.email
+            password.value = val.password
+        }
+        else{
+            resetInput()
+        }
 
         fetchData()
     }
@@ -80,7 +136,7 @@
         </div>
 
         <div class="flex items-center justify-end gap-5">
-            <button @click="toggleModal('add')" class="flex gap-2 items-center p-2 leading-relaxed tracking-wide text-sm bg-blue-500 text-white rounded-lg">
+            <button @click="toggleModal('Add')" class="flex gap-2 items-center p-2 leading-relaxed tracking-wide text-sm bg-blue-500 text-white rounded-lg">
                 <fa icon="fas fa-plus"></fa>
                 Tambahkan dosen baru
             </button>
@@ -125,11 +181,11 @@
                         <td>{{ new DateConverter(x.updated_at).format() }}</td>
                         <td class="text-white justify-center h-full">
                             <div class="flex items-center gap-1">
-                                <button class="bg-orange-400 p-2 rounded-lg h-fit">
+                                <button class="bg-orange-400 p-2 rounded-lg h-fit" @click="toggleModal(x, 'Edit')">
                                     <fa icon="fas fa-edit" fixed-width></fa>
                                 </button>
 
-                                <button class="bg-red-600 p-2 rounded-lg h-fit">
+                                <button @click="hapusDosen(x)" class="bg-red-600 p-2 rounded-lg h-fit">
                                     <fa icon="fas fa-trash" fixed-width></fa>
                                 </button>
                             </div>
@@ -157,11 +213,11 @@
     </div>
 
     <div class="items-center justify-center w-screen h-screen bg-[#000000AA] z-50 absolute top-0 left-0 flex" v-if="isModalOn">
-        <div class="flex flex-col bg-white px-5 py-7 rounded-xl gap-4 w-1/3 justify-center">
+        <div class="flex flex-col bg-white px-5 py-7 rounded-xl gap-4 w-1/3 justify-center" v-if="modalMode == 'Add'"">
             <div class="flex w-full items-center justify-between pb-6">
                 <p class="text-2xl font-medium text-indigo">Tambah Dosen</p>
 
-                <fa @click="toggleModal(0)" icon="fas fa-times" class="text-2xl" fixed-width></fa>
+                <fa @click="toggleModal(0, 'Add')" icon="fas fa-times" class="text-2xl" fixed-width></fa>
             </div>
 
             <div class="flex flex-col gap-8">
@@ -202,6 +258,56 @@
                     <button @click="tambahDosen()" class="w-full flex gap-2 items-center justify-center mt-5 p-3 bg-indigo text-white rounded-lg">
                         <fa icon="fas fa-plus"></fa>
                         Tambahkan
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <div class="flex flex-col bg-white px-5 py-7 rounded-xl gap-4 w-1/3 justify-center" v-if="modalMode == 'Edit'">
+            <div class="flex w-full items-center justify-between pb-6">
+                <p class="text-2xl font-medium text-indigo">Edit Dosen</p>
+
+                <fa @click="toggleModal(0, 'Edit')" icon="fas fa-times" class="text-2xl" fixed-width></fa>
+            </div>
+
+            <div class="flex flex-col gap-8">
+                <div class="flex flex-col items-center w-full justify-center gap-3">
+                    <div class="bg-gray-200 p-4 rounded-xl flex items-center w-full gap-3">
+                        <fa icon="fas fa-user" class="text-gray-500" fixed-width />
+                        <input type="text" class="bg-transparent outline-none w-full" v-model="nama" placeholder="Masukkan nama dosen">
+                    </div>
+                    
+                    <div class="bg-gray-200 p-4 rounded-xl flex items-center w-full gap-3">
+                        <fa icon="fas fa-credit-card" class="text-gray-500" fixed-width />
+                        <input type="text" class="bg-transparent outline-none w-full" v-model="nidn" placeholder="Masukkan NIDN">
+                    </div>
+
+                    <div class="bg-gray-200 p-4 rounded-xl flex items-center w-full gap-3">
+                        <fa icon="fas fa-id-card" class="text-gray-500" fixed-width />
+                        <input type="text" class="bg-transparent outline-none w-full" v-model="jabatan" placeholder="Masukkan Jabatan">
+                    </div>
+
+                    <div class="flex p-4 bg-indigo rounded-lg items-center gap-3 w-full">
+                        <fa icon="fas fa-school" class="text-white"></fa>
+                        <select class="bg-transparent text-white outline-none *:bg-white *:text-black w-full" v-model="prodi">
+                            <option selected hidden value="0">Pilih prodi</option>
+                            <option v-for="x in dataProdi" :value="x.id_prodi">{{ x.nm_prodi }}</option>
+                        </select>
+                    </div>
+
+                    <div class="bg-gray-200 p-4 rounded-xl flex items-center w-full gap-3">
+                        <fa icon="fas fa-envelope" class="text-gray-500" fixed-width />
+                        <input type="text" class="bg-transparent outline-none w-full" v-model="email" placeholder="Masukkan email">
+                    </div>
+
+                    <div class="bg-gray-200 p-4 rounded-xl flex items-center w-full gap-3">
+                        <fa icon="fas fa-lock" class="text-gray-500" fixed-width />
+                        <input type="password" class="bg-transparent outline-none w-full" v-model="password" placeholder="Masukkan password">
+                    </div>
+
+                    <button @click="updateDosen()" class="w-full flex gap-2 items-center justify-center mt-5 p-3 bg-indigo text-white rounded-lg">
+                        <fa icon="fas fa-floppy-disk"></fa>
+                        Update
                     </button>
                 </div>
             </div>
--- blinky-fe/src/pages/Admin/TableMatkul.vue
+++ blinky-fe/src/pages/Admin/TableMatkul.vue
@@ -6,16 +6,23 @@
     import apiClient from '@/services/api';
 
     const dataMatkul = ref()
+    const temp_data = ref()
 
     const nama = ref()
 
     const isModalOn = ref(false)
+    const modalMode = ref('Add')
 
     async function fetchData(){
         await apiClient.get('matkul')
         .then(resp => {
+            console.log(resp.data)
             dataMatkul.value = resp.data
         })
+    }
+
+    async function resetInput(){
+        nama.value = ''
     }
 
     async function tambahMatkul(){
@@ -26,18 +33,62 @@
         })
         .then(resp => {
             if(resp.data == "Success!"){
-                alert('Data matkul gagal didaftarkan!')
-            }
-            else{
                 alert('Data matkul berhasil didaftarkan!')
             }
+            else{
+                alert('Data matkul gagal didaftarkan!')
+            }
 
-            toggleModal(0)
+            resetInput()
+            toggleModal(0, 'Add')
         })
     }
 
-    function toggleModal(val){
+    async function updateMatkul(){
+        await apiClient.get('matkul/update', {
+            params: {
+                'id_matkul' : temp_data.value.id_matkul,
+                'nm_matkul' : nama.value,
+            }
+        })
+        .then(resp => {
+            console.log(resp.data)
+
+            toggleModal(0, 'Edit')
+        })
+    }
+
+    async function hapusMatkul(x){
+        await apiClient.get('matkul/delete', {
+            params: {
+                'id_matkul': x.id_matkul,
+            }
+        })
+        .then(resp => {
+            if(resp.data == "Success!"){
+                alert('Data matkul berhasil dihapus!')
+                fetchData();
+            }
+            else{
+                alert('Data matkul gagal dihapus!')
+            }
+
+            toggleModal(0, 'Delete')
+        })
+    }
+
+    function toggleModal(val, mode){
         isModalOn.value = !isModalOn.value
+        modalMode.value = mode
+
+        temp_data.value = val
+
+        if(mode == 'Edit'){
+            nama.value = val.nm_matkul
+        }
+        else{
+            resetInput()
+        }
 
         fetchData()
     }
@@ -64,7 +115,7 @@
         </div>
 
         <div class="flex items-center justify-end gap-5">
-            <button @click="toggleModal('add')" class="flex gap-2 items-center p-2 leading-relaxed tracking-wide text-sm bg-blue-500 text-white rounded-lg">
+            <button @click="toggleModal('Add')" class="flex gap-2 items-center p-2 leading-relaxed tracking-wide text-sm bg-blue-500 text-white rounded-lg">
                 <fa icon="fas fa-plus"></fa>
                 Tambahkan mata kuliah baru
             </button>
@@ -103,11 +154,11 @@
                         <td>{{ new DateConverter(x.updated_at).format() }}</td>
                         <td class="text-white justify-center h-full">
                             <div class="flex items-center gap-1">
-                                <button class="bg-orange-400 p-2 rounded-lg h-fit">
+                                <button class="bg-orange-400 p-2 rounded-lg h-fit" @click="toggleModal(x, 'Edit')">
                                     <fa icon="fas fa-edit" fixed-width></fa>
                                 </button>
 
-                                <button class="bg-red-600 p-2 rounded-lg h-fit">
+                                <button @click="hapusMatkul(x)" class="bg-red-600 p-2 rounded-lg h-fit">
                                     <fa icon="fas fa-trash" fixed-width></fa>
                                 </button>
                             </div>
@@ -135,11 +186,11 @@
     </div>
 
     <div class="items-center justify-center w-screen h-screen bg-[#000000AA] z-50 absolute top-0 left-0 flex" v-if="isModalOn">
-        <div class="flex flex-col bg-white px-5 py-7 rounded-xl gap-4 w-1/3 justify-center">
+        <div class="flex flex-col bg-white px-5 py-7 rounded-xl gap-4 w-1/3 justify-center" v-if="modalMode == 'Add'"">
             <div class="flex w-full items-center justify-between pb-6">
                 <p class="text-2xl font-medium text-indigo">Tambah Mata Kuliah</p>
 
-                <fa @click="toggleModal(0)" icon="fas fa-times" class="text-2xl" fixed-width></fa>
+                <fa @click="toggleModal(0, 'Add')" icon="fas fa-times" class="text-2xl" fixed-width></fa>
             </div>
 
             <div class="flex flex-col gap-8">
@@ -152,6 +203,28 @@
                     <button @click="tambahMatkul()" class="w-full flex gap-2 items-center justify-center mt-5 p-3 bg-indigo text-white rounded-lg">
                         <fa icon="fas fa-plus"></fa>
                         Tambahkan
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <div class="flex flex-col bg-white px-5 py-7 rounded-xl gap-4 w-1/3 justify-center" v-if="modalMode == 'Edit'">
+            <div class="flex w-full items-center justify-between pb-6">
+                <p class="text-2xl font-medium text-indigo">Edit Mata Kuliah</p>
+
+                <fa @click="toggleModal(0, 'Edit')" icon="fas fa-times" class="text-2xl" fixed-width></fa>
+            </div>
+
+            <div class="flex flex-col gap-8">
+                <div class="flex flex-col items-center w-full justify-center gap-3">
+                    <div class="bg-gray-200 p-4 rounded-xl flex items-center w-full gap-3">
+                        <fa icon="fas fa-book" class="text-gray-500" fixed-width />
+                        <input type="text" class="bg-transparent outline-none w-full" v-model="nama" placeholder="Masukkan nama mata kuliah">
+                    </div>
+
+                    <button @click="updateMatkul()" class="w-full flex gap-2 items-center justify-center mt-5 p-3 bg-indigo text-white rounded-lg">
+                        <fa icon="fas fa-floppy-disk"></fa>
+                        Update
                     </button>
                 </div>
             </div>
